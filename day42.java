781a526ce64324c9990b3a7fc1d7964dfe2c7223
pravakarbhattacharya1234 <102740564+pravakarbhattacharya1234@users.noreply.github.com>
2023-06-07 19:18:39 +0530

    Add files via upload

---
+++ day42.java
@@ -0,0 +1,27 @@
+// 837. New 21 Game
+
+class Solution {
+    public double new21Game(int N, int K, int maxPts) {
+       
+        if (K == 0) return 1.0;
+        if (N >= K - 1 + maxPts) return 1.0;
+
+       
+        double[] dp = new double[N + 1];
+        Arrays.fill(dp, 0.0);
+
+        double probability = 0.0; 
+        double windowSum = 1.0; 
+        dp[0] = 1.0;
+        for (int i = 1; i <= N; i++) {
+            dp[i] = windowSum / maxPts;
+
+            if (i < K) windowSum += dp[i];
+            else probability += dp[i];
+
+            if (i >= maxPts) windowSum -= dp[i - maxPts];
+        }
+
+        return probability;
+    }
+}
