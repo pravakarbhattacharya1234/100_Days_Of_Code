58d3ce2f6703b824ffeb5c983cfb260030d85994
pravakarbhattacharya1234 <102740564+pravakarbhattacharya1234@users.noreply.github.com>
2023-06-10 11:20:45 +0530

    Add files via upload

---
+++ day54.java
@@ -0,0 +1,17 @@
+// 1502. Can Make Arithmetic Progression From Sequence
+
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        int diff = arr[1] - arr[0];
+        
+        for (int i = 2; i < n; i++) {
+            if ((arr[i] - arr[i - 1]) != diff) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
